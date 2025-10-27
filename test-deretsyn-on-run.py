043065e64bfe45d33a838c3c06f270c1e98af7e4
Kiran Bhattacharyya <kiran.bhattacharyya@intusurg.com>
2025-10-27 13:05:14 -0700

    run orchestrator once

---
+++ test-deretsyn-on-run.py
@@ -0,0 +1,27 @@
+from utils.agents import orchestrator_straight_run, DeRetSynState
+from utils.index_w_faiss import FaissReader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+faiss_reader = FaissReader("surgical_faiss_index")
+
+MODEL_NAME = os.getenv('TOGETHER_LLAMA32')
+API_KEY = os.getenv('TOGETHER_API_KEY')
+BASE_URL = os.getenv('TOGETHER_URL')
+
+state = DeRetSynState(
+    original_question="What is the most common cause of acute appendicitis?",
+    model=MODEL_NAME,
+    api_key=API_KEY,
+    base_url=BASE_URL,
+    faiss_index_path="surgical_faiss_index",
+    verbose=True,
+    iterations=0,
+    wikipedia_results="",
+    run_async=True,
+    vectorstore=faiss_reader
+)
+
+final_state = orchestrator_straight_run(state)
+print(final_state)
